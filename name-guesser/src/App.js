bb37505f290122be55102ef57b52eed0103c7bf0
Roy Rosario <roy.rosario88@gmail.com>
2021-09-10 20:12:26 -0400

    rewrote App.js
    Also started on a new component called NameLogic.js

---
+++ name-guesser/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NameLogic from './NameLogic.js'
+
+function App() {
+  return (
+    <NameLogic />
+  );
+}
+
+export default App;
+
+
---
+++ name-guesser/src/NameLogic.js
@@ -0,0 +1,10 @@
+import React, {useState}from 'react'
+
+function NameLogic(){
+    
+    return(
+        <h1>Name Checker</h1>
+    )
+}
+
+export default NameLogic
