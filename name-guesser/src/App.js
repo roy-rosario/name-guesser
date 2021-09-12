e706b06175fb65c25fe4938ddff7a2c8d49b152a
Roy Rosario <roy.rosario88@gmail.com>
2021-09-12 13:50:56 -0400

    further dev

--- name-guesser/src/App.js
+++ name-guesser/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import NameLogic from './NameLogic.js'
 
--- name-guesser/src/NameLogic.js
+++ name-guesser/src/NameLogic.js
@@ -1,9 +1,40 @@
 import React, {useState}from 'react'
 
 function NameLogic(){
+
+    const [ nameInfo, setNameInfo] = useState(
+        {
+            name: "",
+            gender: "",
+            probability: ""
+        })
+
+    async function getData(){    
+        
+        try{
+            const res = await fetch(`https://api.genderize.io?name=${nameInfo.name}`)
+            const data = await res.json() 
+            setNameInfo({
+            name: data.name,
+            gender: data.gender,
+            probability: data.probability
+            })
+        }catch(err){
+            console.error(err)
+        }
+    }
     
     return(
+        <>
         <h1>Name Checker</h1>
+        <button onClick = {getData}>click for info</button>
+        {/* <form className = "form">
+            <input 
+                type = 'text' 
+                name = "nameInput"
+                value = {nameInput} />
+        </form> */}
+        </>
     )
 }
 
